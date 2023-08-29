29146fa7ff4cf8fa3105f59738f0448f8b617d42
lucasdev2001 <lucasdev2001@gmail.com>
2023-08-29 02:37:47 -0300

    add minio integration

--- backend/src/routes/files.ts
+++ backend/src/routes/files.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 const router = express.Router();
 
-const minioClient = new Minio.Client({
+const minio = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
   port: 9000,
   useSSL: false,
@@ -17,16 +17,32 @@ const minioClient = new Minio.Client({
 
 router.post("/:bucket", upload.single("file"), async (req, res) => {
   const bucket = req.params.bucket;
-  console.log(bucket);
 
-  const objRes = await minioClient.putObject(
+  const objRes = await minio.putObject(
     bucket,
     req.file.originalname,
     req.file.buffer
   );
+
   console.log(objRes);
 
   res.status(200).json({ message: "ok" });
+});
+
+router.get("/:bucket", async (req, res) => {
+  const bucket = req.params.bucket;
+
+  var data: object[] = [];
+  var stream = minio.listObjects(bucket, "", true, { IncludeVersion: true });
+  stream.on("data", function (obj) {
+    data.push(obj);
+  });
+  stream.on("end", function (obj: any) {
+    res.status(200).json(data);
+  });
+  stream.on("error", function (err) {
+    res.status(400).json({ message: err.message });
+  });
 });
 
 export default router;
