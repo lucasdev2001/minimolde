02c253aa5365603a793e73b4a5ab877824510ed3
lucasdev2001 <lucasdev2001@gmail.com>
2023-08-31 03:59:29 -0300

    add layout

--- client/src/routes/Home.vue
+++ client/src/routes/Home.vue
@@ -21,8 +21,12 @@ const logOut = () => {
 <template>
   <div className="navbar bg-base-100">
     <div className="flex-1">
-      <a className="btn btn-ghost normal-case text-xl"></a>
-      <label for="my-drawer-2" class="drawer-button">Open drawer</label>
+      <button class="btn btn-ghost btn-circle">
+        <div class="indicator">
+          <i class="fa-regular fa-bell text-lg"></i>
+          <span class="badge badge-xs badge-primary indicator-item"></span>
+        </div>
+      </button>
     </div>
     <div className="flex-none">
       <div className="dropdown dropdown-end">
@@ -39,6 +43,11 @@ const logOut = () => {
           className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
+            <label for="my-drawer-2" class="drawer-button cursor-pointer"
+              >menu</label
+            >
+          </li>
+          <li>
             <a className="justify-between"> Profile </a>
           </li>
           <li><a>Settings</a></li>
@@ -48,16 +57,57 @@ const logOut = () => {
     </div>
   </div>
 
-  <div class="drawer">
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col items-center justify-center"></div>
-    <div class="drawer-side">
-      <label for="my-drawer-2" class="drawer-overlay"></label>
-      <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul>
-    </div>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Job</th>
+          <th>Favorite Color</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr class="bg-base-200">
+          <th>1</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+        </tr>
+        <!-- row 2 -->
+        <tr>
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+        <!-- row 3 -->
+        <tr>
+          <th>3</th>
+          <td>Brice Swyre</td>
+          <td>Tax Accountant</td>
+          <td>Red</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
+  <label for="my-drawer-2" class="drawer-button">
+    <div class="drawer drawer-end">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div
+        class="drawer-content flex flex-col items-center justify-center"
+      ></div>
+      <div class="drawer-side">
+        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <!-- Sidebar content here -->
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+  </label>
 </template>
