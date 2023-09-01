44d9e61874b54b402118fdea82f0e7ab69f82066
lucasdev2001 <lucasdev2001@gmail.com>
2023-09-01 02:04:34 -0300

    add lateral bar

--- client/src/routes/Home.vue
+++ client/src/routes/Home.vue
@@ -19,8 +19,8 @@ const logOut = () => {
 </script>
 
 <template>
-  <div className="navbar bg-base-100">
-    <div className="flex-1">
+  <nav className="navbar bg-base-100">
+    <div className="navbar-start">
       <button class="btn btn-ghost btn-circle">
         <div class="indicator">
           <i class="fa-regular fa-bell text-lg"></i>
@@ -28,7 +28,8 @@ const logOut = () => {
         </div>
       </button>
     </div>
-    <div className="flex-none">
+
+    <div className="navbar-end">
       <div className="dropdown dropdown-end">
         <label tabIndex="{0}" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
@@ -43,7 +44,7 @@ const logOut = () => {
           className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
-            <label for="my-drawer-2" class="drawer-button cursor-pointer"
+            <label for="drawer" class="drawer-button cursor-pointer"
               >menu</label
             >
           </li>
@@ -55,12 +56,12 @@ const logOut = () => {
         </ul>
       </div>
     </div>
-  </div>
+  </nav>
 
   <div class="overflow-x-auto">
     <table class="table">
       <!-- head -->
-      <thead>
+      <thead class="bg-base-200">
         <tr>
           <th></th>
           <th>Name</th>
@@ -70,21 +71,21 @@ const logOut = () => {
       </thead>
       <tbody>
         <!-- row 1 -->
-        <tr class="bg-base-200">
+        <tr class="hover:bg-base-200">
           <th>1</th>
           <td>Cy Ganderton</td>
           <td>Quality Control Specialist</td>
           <td>Blue</td>
         </tr>
         <!-- row 2 -->
-        <tr>
+        <tr class="hover:bg-base-200">
           <th>2</th>
           <td>Hart Hagerty</td>
           <td>Desktop Support Technician</td>
           <td>Purple</td>
         </tr>
         <!-- row 3 -->
-        <tr>
+        <tr class="hover:bg-base-200">
           <th>3</th>
           <td>Brice Swyre</td>
           <td>Tax Accountant</td>
@@ -94,20 +95,30 @@ const logOut = () => {
     </table>
   </div>
 
-  <label for="my-drawer-2" class="drawer-button">
-    <div class="drawer drawer-end">
-      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-      <div
-        class="drawer-content flex flex-col items-center justify-center"
-      ></div>
-      <div class="drawer-side">
-        <label for="my-drawer-2" class="drawer-overlay"></label>
-        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-          <!-- Sidebar content here -->
-          <li><a>Sidebar Item 1</a></li>
-          <li><a>Sidebar Item 2</a></li>
-        </ul>
-      </div>
+  <div class="drawer drawer-end">
+    <input id="drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center justify-center"></div>
+    <div class="drawer-side">
+      <label for="drawer" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <!-- Sidebar content here -->
+        <label for="drawer" class="drawer-button">
+          <li>
+            <a>
+              <i class="fa-solid fa-list-check"></i>
+              tasks
+            </a>
+          </li>
+        </label>
+        <label for="drawer" class="drawer-button">
+          <li>
+            <a>
+              <i class="fa-solid fa-box-archive"></i>
+              files
+            </a>
+          </li>
+        </label>
+      </ul>
     </div>
-  </label>
+  </div>
 </template>
